1f775aa66b8ecfac43b8c8ded6959427b02ce13c
Tomoaki Otsuka <tom__bo@Tomoaki-no-MacBook-Air.local>
2013-11-13 11:10:00 +0900

    add user reverse

--- main.c
+++ main.c
@@ -24,12 +24,8 @@ char *board[10] = {
 
 
 int isOut(int x, int y){
-	if(x<1 || y<1 || x>15 || y>8) return 1;
+	if(x<1 || y<1 || x>16 || y>8) return 1;
 	else return 0;
-}
-
-int canReverse(int x, int y){
-	return 0;
 }
 
 int countDisc(){
@@ -70,6 +66,34 @@ int canPut(int x, int y, char p){
 	return 0;
 }
 
+int put(int x, int y, char p){
+	int i=0,j,k;
+	int x0, y0, x2, y2, x3, y3;
+	for(;i<8;i++){
+		x0=x+dir[i][0];
+		y0=y+dir[i][1];
+		if(isOut(x0, y0)) continue;
+		else if(board[y0][x0]=='.') continue;
+		else if(board[y0][x0]==p) continue;
+		else{ 
+			for(j=1;j<16;j++){
+				x2=x0+dir[i][0]*j;
+				y2=y0+dir[i][1]*j;
+				if(isOut(x2, y2)) continue;
+				else if(board[y2][x2] == '.') continue;
+				else if(board[y2][x2] == p){
+					for(k=-1;k<j;k++){
+						x3=x0+dir[i][0]*k;
+						y3=y0+dir[i][1]*k;
+						board[y3][x3] = p;
+					}
+				}
+				else continue;
+			}
+		}
+	}
+	return 0;
+}
 
 
 int output(int counter_val, int color, int cursol_x, int cursol_y) {
@@ -89,31 +113,19 @@ int output(int counter_val, int color, int cursol_x, int cursol_y) {
 	}else{
 		lcd_locate(20,12);
 		lcd_printf("%d",counter_val);
+		board[cursol_y][cursol_x] = '<';
 
-		board[cursol_y][cursol_x] = '@';
-
-		lcd_locate(1,1);
-		lcd_printf(board[0]);
-		lcd_locate(1,2);
-		lcd_printf(board[1]);
-		lcd_locate(1,3);
-		lcd_printf(board[2]);
-		lcd_locate(1,4);
-		lcd_printf(board[3]);
-		lcd_locate(1,5);
-		lcd_printf(board[4]);
-		lcd_locate(1,6);
-		lcd_printf(board[5]);
-		lcd_locate(1,7);
-		lcd_printf(board[6]);
-		lcd_locate(1,8);
-		lcd_printf(board[7]);
-		lcd_locate(1,9);
-		lcd_printf(board[8]);
-		lcd_locate(1,10);
-		lcd_printf(board[9]);
-		lcd_locate(0,12);
-		lcd_printf("x, y = %d, %d", cursol_x, cursol_y);
+		lcd_locate(1,1); lcd_printf(board[0]);
+		lcd_locate(1,2); lcd_printf(board[1]);
+		lcd_locate(1,3); lcd_printf(board[2]);
+		lcd_locate(1,4); lcd_printf(board[3]);
+		lcd_locate(1,5); lcd_printf(board[4]);
+		lcd_locate(1,6); lcd_printf(board[5]);
+		lcd_locate(1,7); lcd_printf(board[6]);
+		lcd_locate(1,8); lcd_printf(board[7]);
+		lcd_locate(1,9); lcd_printf(board[8]);
+		lcd_locate(1,10); lcd_printf(board[9]);
+		lcd_locate(0,12); lcd_printf("x, y = %d, %d", cursol_x, cursol_y);
 
 		board[cursol_y][cursol_x] = ' ';
 	}
@@ -130,28 +142,22 @@ int main() {
 	// キーとカウンタ取得
 		int counter_val=*counter_reg;
 		char key_val=*key_reg;
+
 		// キー入力条件(WSADJK)
 		if( key_val & 0x20){         //w
 		}else if( key_val & 0x10){   //s
-			cursol_x-=2;
+			if(!isOut(cursol_x-2, cursol_y)) cursol_x-=2;
 		}else if( key_val & 0x08){   //a
-			if(canPut(cursol_x-1, cursol_y, 'o')) board[cursol_y][cursol_x-1] = 'o';
-			else{
-				lcd_locate(0,0);
-				lcd_printf("Can't Put!!");
-			}
+			put(cursol_x-1, cursol_y, 'o');
+			// if(canPut(cursol_x-1, cursol_y, 'o')) board[cursol_y][cursol_x-1] = 'o';
+			// else{
+			// }
 		}else if( key_val & 0x04){   //d
-				lcd_locate(0,0);
-				lcd_printf("!!");
-			cursol_x+=2;
+			if(!isOut(cursol_x+2, cursol_y)) cursol_x+=2;
 		}else if( key_val & 0x02){   //j
-				lcd_locate(0,0);
-				lcd_printf("!!");
-			cursol_y++;
+			if(!isOut(cursol_x, cursol_y+1)) cursol_y++;
 		}else if( key_val & 0x01){   //k
-				lcd_locate(0,0);
-				lcd_printf("!!");
-			cursol_y--;
+			if(!isOut(cursol_x, cursol_y-1)) cursol_y--;
 		}
 		output(counter_val, color, cursol_x, cursol_y);
 	}
